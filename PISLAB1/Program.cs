242b95286b989ee7b124aace9db123a87f69f76d
Goranbogetic <Goran.Bogetic.00@fesb.hr>
2023-03-15 17:24:46 +0100

    New commit

--- PISLAB1/Program.cs
+++ PISLAB1/Program.cs
@@ -7,6 +7,7 @@ namespace PISLAB1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Ima meda.");
         }
     }
 }
